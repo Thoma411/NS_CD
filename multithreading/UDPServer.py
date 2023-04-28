bd836ef2ed2be4ce14bfedecac3381602c741a94
Thoma411 <NADTM_Thomax11@163.com>
2023-04-28 21:01:08 +0800

    尝试在part_socket的基础上编写多线程(UDP)

---
+++ multithreading/UDPServer.py
@@ -0,0 +1,50 @@
+'''
+Author: Thoma4
+Date: 2022-01-03 01:01:46
+LastEditTime: 2023-04-28 20:58:28
+Description: 
+'''
+# UDPserver
+from socket import *
+import time as tm
+import threading as th
+
+
+def sSend(sk, dstIP, dstPort):
+    while True:
+        sMsgSend = input('[s]send msg: ')
+        sk.sendto(sMsgSend.encode(), (dstIP, dstPort))
+
+
+def sRecv(sk, MAX_SIZE):
+    while True:
+        sMsgRecv = sk.recvfrom(MAX_SIZE)
+        print(f'\n[s]recv msg: {str(sMsgRecv[1])}{sMsgRecv[0].decode()}')
+
+
+def sMain():
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)  # 创建套接字
+    MAX_SIZE = 2048
+    opt_closeServer = 'shutdown'
+    s_t = ''  # 服务端时间
+    serverSocket.bind(('', serverPort))  # 绑定IP/端口
+    s_t = tm.strftime("%H:%M:%S", tm.localtime())
+    print(f'[s]({s_t})The server is ready to receive...')
+
+    while True:
+        SmsgRecv, CAddr = serverSocket.recvfrom(MAX_SIZE)
+        SmsgModified = SmsgRecv.decode().upper()
+        s_t = tm.strftime("%H:%M:%S", tm.localtime())
+        print(f'[s]({s_t})msg:', SmsgModified, 'recvfrom:', CAddr)
+        serverSocket.sendto(SmsgModified.encode(), CAddr)
+        if SmsgModified == opt_closeServer.upper():
+            s_t = tm.strftime("%H:%M:%S", tm.localtime())
+            print(
+                f'[s]({s_t})Server has been shut down remotely, operator:', CAddr)
+            serverSocket.close()
+            break
+
+
+if __name__ == '__main__':
+    sMain()
