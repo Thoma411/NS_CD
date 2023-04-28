acd17c582dc21d9161e1c7b884434ce56e811f0e
Thoma411 <NADTM_Thomax11@163.com>
2023-04-28 23:38:51 +0800

    同步UDP C/S端

--- multithreading/UDPClient.py
+++ multithreading/UDPClient.py
@@ -1,11 +1,25 @@
 '''
 Author: Thoma4
 Date: 2022-01-03 00:55:14
-LastEditTime: 2023-04-28 20:44:38
+LastEditTime: 2023-04-28 23:35:30
 Description: hotspot 192.168.137.1
 '''
 from socket import *
-import time as tm
+import threading as th
+import sharedMethods as sm  # 自定义c/s共用方法
+
+
+def cSend(sk, dstIP, dstPort):
+    while True:
+        cMsgSend = input('[c]send msg: ')
+        sk.sendto(cMsgSend.encode(), (dstIP, dstPort))
+
+
+def cRecv(sk, MAX_SIZE):
+    while True:
+        cMsgRecv = sk.recvfrom(MAX_SIZE)
+        print(f'\n[c]recv msg: {str(cMsgRecv[1])}{cMsgRecv[0].decode()}')
+
 
 def cMain():
     serverName = '192.168.137.1'
@@ -16,20 +30,24 @@ def cMain():
     c_t = ''  # 客户端时间
 
     while True:
-        c_t = tm.strftime("%H:%M:%S", tm.localtime())
-        CmsgSend = input(f'[c]({c_t})Input lowercase sentense: ')
+        CmsgSend = input(f'[c]({sm.gt(c_t)})Input lowercase sentense: ')
         if not CmsgSend:
             break
         clientSocket.sendto(CmsgSend.encode(), (serverName, serverPort))
         CmsgRecv, SAddr = clientSocket.recvfrom(MAX_SIZE)
         CmsgModified = CmsgRecv.decode()
-        c_t = tm.strftime("%H:%M:%S", tm.localtime())
-        print(f'[c]({c_t})msg:', CmsgModified, 'recvfrom:', SAddr)
+        print(f'[c]({sm.gt(c_t)})msg:', CmsgModified, 'recvfrom:', SAddr)
         if CmsgSend == opt_closeServer:  # 远程关闭服务端
-            c_t = tm.strftime("%H:%M:%S", tm.localtime())
-            print(f'[c]({c_t})Server has been shut down.')
+            print(f'[c]({sm.gt(c_t)})Server has been shut down.')
             break
     clientSocket.close()
+
+    # dest_IP, dest_Port = '192.168.137.1', 12000
+    # tcSend = th.Thread(target=cSend, args=(clientSocket, dest_IP, dest_Port))
+    # tcRecv = th.Thread(target=cRecv, args=(clientSocket, MAX_SIZE))
+    # tcSend.start()
+    # tcRecv.start()
+
 
 if __name__ == '__main__':
     cMain()
--- multithreading/UDPServer.py
+++ multithreading/UDPServer.py
@@ -1,13 +1,13 @@
 '''
 Author: Thoma4
 Date: 2022-01-03 01:01:46
-LastEditTime: 2023-04-28 21:04:50
+LastEditTime: 2023-04-28 23:35:20
 Description: sSend()/sRecv()未启用
 '''
 # UDPserver
 from socket import *
-import time as tm
 import threading as th
+import sharedMethods as sm  # 自定义c/s共用方法
 
 
 def sSend(sk, dstIP, dstPort):
@@ -29,27 +29,24 @@ def sMain():
     opt_closeServer = 'shutdown'
     s_t = ''  # 服务端时间
     serverSocket.bind(('', serverPort))  # 绑定IP/端口
-    s_t = tm.strftime("%H:%M:%S", tm.localtime())
-    print(f'[s]({s_t})The server is ready to receive...')
+    print(f'[s]({sm.gt(s_t)})The server is ready to receive...')
 
     while True:
         SmsgRecv, CAddr = serverSocket.recvfrom(MAX_SIZE)
         SmsgModified = SmsgRecv.decode().upper()
-        s_t = tm.strftime("%H:%M:%S", tm.localtime())
-        print(f'[s]({s_t})msg:', SmsgModified, 'recvfrom:', CAddr)
+        print(f'[s]({sm.gt(s_t)})msg:', SmsgModified, 'recvfrom:', CAddr)
         serverSocket.sendto(SmsgModified.encode(), CAddr)
         if SmsgModified == opt_closeServer.upper():
-            s_t = tm.strftime("%H:%M:%S", tm.localtime())
             print(
-                f'[s]({s_t})Server has been shut down remotely, operator:', CAddr)
+                f'[s]({sm.gt(s_t)})Server has been shut down remotely, operator:', CAddr)
             serverSocket.close()
             break
 
     # dest_IP, dest_Port = '192.168.137.1', 12000
-    # tSend = th.Thread(target=sSend, args=(serverSocket, dest_IP, dest_Port))
-    # tRecv = th.Thread(target=sRecv, args=(serverSocket, MAX_SIZE))
-    # tSend.start()
-    # tRecv.start()
+    # tsSend = th.Thread(target=sSend, args=(serverSocket, dest_IP, dest_Port))
+    # tsRecv = th.Thread(target=sRecv, args=(serverSocket, MAX_SIZE))
+    # tsSend.start()
+    # tsRecv.start()
 
 
 if __name__ == '__main__':
