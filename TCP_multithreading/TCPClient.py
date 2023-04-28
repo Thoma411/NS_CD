8c95de83524569f4be1a05f450eab46c303c6d6d
Thoma411 <NADTM_Thomax11@163.com>
2023-04-28 17:34:42 +0800

    实现了多线程，取消了客户端关闭服务端的关键字

--- TCP_multithreading/TCPClient.py
+++ TCP_multithreading/TCPClient.py
@@ -1,8 +1,8 @@
 '''
 Author: Thoma4
 Date: 2022-01-03 01:09:56
-LastEditTime: 2023-04-28 16:52:36
-Description: single-thread C
+LastEditTime: 2023-04-28 17:34:06
+Description: multithreading C
 '''
 from socket import *
 import sharedMethods as sm  # 自定义c/s共用方法
@@ -12,7 +12,6 @@ def cMain():
     serverName = '192.168.137.1'  # 热点局域网IP
     serverPort = 12000
     MAX_SIZE = 1024
-    opt_closeServer = 'shutdown'  # 操作关键词
     c_t = ''  # 客户端时间
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))  # 连接server
@@ -23,9 +22,6 @@ def cMain():
             print(f'[c]({sm.gt(c_t)})disconnected.')
             break
         clientSocket.send(CmsgSend.encode())
-        if CmsgSend == opt_closeServer:  # 远程关闭服务端
-            print(f'[c]({sm.gt(c_t)})Server has been shut down.')
-            break
         CmsgRecv = clientSocket.recv(MAX_SIZE)
         CmsgModified = CmsgRecv.decode()  # 解码
         print(f'[c]({sm.gt(c_t)})msg:', CmsgModified, 'recvfrom:', serverName)
