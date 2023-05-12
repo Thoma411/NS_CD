a8d7f7314a37943a8da830471aa6da1dfbdfcdf0
Thoma411 <NADTM_Thomax11@163.com>
2023-05-12 12:19:28 +0800

    增加了ticket的定义和部分方法

--- cryptMethod/messageFormat.py
+++ cryptMethod/messageFormat.py
@@ -1,8 +1,8 @@
 '''
 Author: Thoma411
 Date: 2023-05-10 22:23:04
-LastEditTime: 2023-05-11 23:59:32
-Description: 
+LastEditTime: 2023-05-12 12:17:22
+Description: message definition
 '''
 import datetime as dt
 import random as rd
@@ -46,6 +46,57 @@ def msg_rndKey(dgt: int = 8):  # 生成定长随机字符串,默认8位
     return rnd_str
 
 
+class TICKET:  # ticket内部字段定义
+    ID_SRC = 00  # 源编号
+    AD_SRC = ''  # 源地址
+    ID_DST = ''  # 目标编号
+    LT_2 = ''  # *AS2C生成的有效期
+    KEY_SHARE = None  # 共享密钥
+    TS_2 = None  # *AS2C生成的时间戳
+
+    def __init__(self, id_src, ad_src, id_dst, lt_2, key_share=None, ts_2=None):
+        self.ID_SRC = id_src
+        self.AD_SRC = ad_src
+        self.ID_DST = id_dst
+        self.LT_2 = lt_2
+        if key_share is not None:  # 判断类型并初始化 防止潜在类型报错
+            self.KEY_SHARE = key_share
+        else:
+            self.KEY_SHARE = msg_rndKey()
+        if ts_2 is not None:
+            self.TS_2 = ts_2
+        else:
+            self.TS_2 = msg_getTime()
+
+    @classmethod
+    def getTicket(cls, tkt: str):  # 接收票据
+        key_share = tkt[:LEN_KEY]
+        id_src = tkt[LEN_KEY:LEN_KEY + LEN_ID]
+        ad_src = tkt[LEN_KEY + LEN_ID:LEN_KEY + LEN_ID + LEN_AD]
+        id_dst = tkt[LEN_KEY + LEN_ID + LEN_AD:
+                     LEN_KEY + LEN_ID + LEN_AD + LEN_ID]
+        ts_2 = tkt[LEN_KEY + LEN_ID + LEN_AD + LEN_ID:
+                   LEN_KEY + LEN_ID + LEN_AD + LEN_ID + LEN_TS]
+        lt_2 = tkt[LEN_KEY + LEN_ID + LEN_AD + LEN_ID + LEN_TS:
+                   LEN_KEY + LEN_ID + LEN_AD + LEN_ID + LEN_TS + LEN_LT]
+        MSG_TICKET = TICKET(id_src, ad_src, id_dst, lt_2, key_share, ts_2)
+        return MSG_TICKET
+
+    def show(self):
+        print(f'KEY_SHARE: {self.KEY_SHARE}, ID_SRC: {self.ID_SRC},', end=' ')
+        print(f'AD_SRC: {self.AD_SRC}, ID_DST: {self.ID_DST},', end=' ')
+        print(f'TS_2: {self.TS_2}, LT_2: {self.LT_2}')
+
+    def concatmsg(self):  # 拼接各字段
+        MSG_TICKET = self.KEY_SHARE + \
+            msg_i2s(self.ID_SRC, LEN_ID) + \
+            msg_i2s(self.AD_SRC, LEN_AD) + \
+            msg_i2s(self.ID_DST, LEN_ID) +\
+            msg_i2s(self.TS_2, LEN_TS) +\
+            msg_i2s(self.LT_2, LEN_LT)
+        return MSG_TICKET
+
+
 class C2AS:  # C->AS 报文字段定义
     ID_C = 00
     ID_TGS = 00
@@ -78,14 +129,14 @@ class C2AS:  # C->AS 报文字段定义
 
     @updateT.getter
     def updateT(self):
-        self.TS_1 = msg_getTime()
+        self.TS_1 = msg_getTime()  # 即时更新时间戳
         return self.TS_1
 
     def show(self):
         self.updateT
         print(f'ID_C: {self.ID_C}, ID_TGS: {self.ID_TGS}, TS_1: {self.TS_1}')
 
-    def concatmsg(self):
+    def concatmsg(self):  # 拼接各字段
         MSG_C2AS = msg_i2s(self.ID_C, LEN_ID) + \
             msg_i2s(self.ID_TGS, LEN_ID) + \
             msg_i2s(self.TS_1, LEN_TS)
@@ -140,8 +191,8 @@ class AS2C:  # AS->C 报文字段定义
 
     def show(self):
         self.updateT
-        print(
-            f'K_C_TGS: {self.K_C_TGS}, ID_TGS: {self.ID_TGS}, TS_2: {self.TS_2}, LT_2: {self.LT_2}')
+        print(f'K_C_TGS: {self.K_C_TGS}, ID_TGS: {self.ID_TGS},', end=' ')
+        print(f'TS_2: {self.TS_2}, LT_2: {self.LT_2}')
 
     def concatmsg(self):
         MSG_AS2C = self.K_C_TGS + \
@@ -153,7 +204,7 @@ class AS2C:  # AS->C 报文字段定义
 
 if __name__ == '__main__':
     # !由于精度较高,临时生成的时间戳可能导致时间比较不同(ticket里的和ticket外的)
-    # *别忘了还有首部没写,记得加上偏移量
+    # *别忘了还有首部没写,记得加上偏移量 / 或者剥去首部再调用类方法
     msg1 = C2AS(1, 1)
     msg1.show()
     m1 = msg1.concatmsg()
@@ -167,11 +218,16 @@ if __name__ == '__main__':
     m2 = msg2.concatmsg()
     print(m2, len(m2))
 
-    msg22 = AS2C.getMsg(m2)
-    msg22.show()
+    # msg22 = AS2C.getMsg(m2)
+    # msg22.show()
     # testmsg1 = C2AS(1, 1)
     # print(testmsg1.updateT)
     # tm.sleep(1)
     # print(testmsg1.updateT)
     # tm.sleep(1)
     # print(testmsg1.TS_1)
+
+    tkt1 = TICKET(1, '127001', 2, 6000)
+    tkt1.show()
+    tkt11 = tkt1.concatmsg()
+    print(tkt11, len(tkt11))
