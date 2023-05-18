4eb952f9f1c0697bb3e27209f8932c7a1a43ec9f
Luckyhao266 <2734008286@qq.com>
2023-05-18 15:34:17 +0800

    添加功能

--- login/message_process.py
+++ login/message_process.py
@@ -3,40 +3,13 @@ import json
 import struct
 from network import send_message
 
+
 # 管理员登录消息处理
 
 
 def admin_on_login(username, password):
     # 构造首部
     head_info = {"head": "01", "type": "01", "subtype": "01",
-                 "timestamp": int(time.time())}  # 生成当前时间戳
-
-    # 计算正文长度并填充冗余位
-    content = {"username": username, "password": password}
-    content_str = json.dumps(content)
-    content_len = len(content_str.encode('utf-8'))
-    redundant = struct.pack('>4s', b'\x00\x00\x00\x00')
-
-    # 组装message1
-    message = head_info
-    message["content_len"] = content_len
-    message["redundant"] = list(redundant)
-    message.update(content)
-    # 发送消息
-    response = send_message('localhost', 10001, message)
-    response1 = response.decode()
-    if response1 == "01":
-        return 1
-
-    else:
-        pass
-
-# 学生登录消息处理
-
-
-def stu_on_login(username, password):
-    # 构造首部
-    head_info = {"head": "01", "type": "01", "subtype": "02",
                  "timestamp": int(time.time())}  # 生成当前时间戳
 
     # 计算正文长度并填充冗余位
@@ -58,3 +31,69 @@ def stu_on_login(username, password):
 
     else:
         pass
+
+
+# 学生登录消息处理
+
+
+def stu_on_login(username, password):
+    # 构造首部
+    head_info = {"head": "01", "type": "01", "subtype": "02",
+                 "timestamp": int(time.time())}  # 生成当前时间戳
+
+    # 计算正文长度并填充冗余位
+    content = {"username": username, "password": password}
+    content_str = json.dumps(content)
+    content_len = len(content_str.encode('utf-8'))
+    redundant = struct.pack('>4s', b'\x00\x00\x00\x00')
+
+    # 组装message1
+    message = head_info
+    message["content_len"] = content_len
+    message["redundant"] = list(redundant)
+    message.update(content)
+    # 发送消息
+    response = send_message('localhost', 10001, message)
+    response1 = response.decode()
+    print("学生登录的回复：")
+    print(response1)
+    if response1 == "01":
+        return 1
+
+    else:
+        pass
+
+
+# 学生成绩的消息处理
+def query_student_score(student_id):
+    # 构建请求消息并发送
+    message = {
+        "student_id": student_id
+    }
+    response = send_message('localhost', 10001, message)
+    print("000")
+    response1 = response.decode()
+    print("001")
+    print(response1)
+    print("002")
+    response_dict = json.loads(response1)
+    print("002")
+    # 接收响应消息并进行解析
+
+    if response_dict.get("error"):
+        raise Exception("查询学生成绩失败：{}".format(response_dict["error"]))
+    else:
+        name = response_dict.get("name")
+        gender = response_dict.get("gender")
+        age = response_dict.get("age")
+        chinese_score = response_dict.get("chinese_score")
+        math_score = response_dict.get("math_score")
+        english_score = response_dict.get("english_score")
+        return {
+            "name": name,
+            "gender": gender,
+            "age": age,
+            "chinese_score": chinese_score,
+            "math_score": math_score,
+            "english_score": english_score
+        }
--- login/server.py
+++ login/server.py
@@ -111,6 +111,10 @@ def handle_client(connection, client_address):
                 if message["password"] == stu_pass:
                     print(stu_pass)
                     connection.sendall("01".encode())  # 进入学生信息查看界面
+                    data = connection.recv(1024)
+                    # 解析数据
+                    message = json.loads(data.decode('utf-8'))
+
                 else:
                     pass
 
@@ -123,8 +127,42 @@ def handle_client(connection, client_address):
             print("Received message 2: name=%s, student_id=%s, chinese_score=%d, math_score=%s, english_score=%s"
                   % (name, student_id, chinese_score, math_score, english_score))
             # 处理第二个数据报文
-        else:
-            print("Unknown message type")
+        elif "student_id" in message and len(message) == 1:
+            student_id = message["student_id"]
+            print("1")
+            # 数据库操作 查询学生成绩
+            db = pymysql.connect(host="127.0.0.1", user="root", passwd="", db="student")  # 打开数据库连接
+            cursor = db.cursor()  # 使用cursor()方法获取操作游标
+            sql = "SELECT name, gender, age, c_grade, m_grade, e_grade FROM student_k WHERE id='%s'" % student_id  # SQL 查询语句
+            try:
+                # 执行SQL语句
+                cursor.execute(sql)
+                # 获取所有记录列表
+                results = cursor.fetchall()
+                if len(results) > 0:
+                    # 取第一个结果
+                    result = results[0]
+                    # 构建成绩字典
+                    scores_dict = {
+                        "name": result[0],
+                        "gender": result[1],
+                        "age": result[2],
+                        "chinese_score": result[3],
+                        "math_score": result[4],
+                        "english_score": result[5]
+                    }
+                    # 发送成绩字典数据给客户端
+                    connection.sendall(json.dumps(scores_dict).encode())
+                else:
+                    # 没有找到学生记录，发送空字典给客户端
+                    connection.sendall(json.dumps({}).encode())
+                    print("没有找到学生记录")
+            except Exception as e:
+                print("查询学生成绩时发生错误。错误消息：", e)
+                # 数据库操作时发生错误，将错误信息发送给客户端
+                connection.sendall(json.dumps({"error": str(e)}).encode())
+            finally:
+                db.close()  # 关闭数据库连接
 
         # 当前请求处理完毕后可以关闭连接
         connection.close()
@@ -135,8 +173,8 @@ def handle_client(connection, client_address):
 
 
 if __name__ == '__main__':
-    SERVER_HOST = '0.0.0.0'
-    SERVER_PORT = 10006
+    SERVER_HOST = 'localhost'
+    SERVER_PORT = 10001
     BUFFER_SIZE = 1024
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
