5cadb62e2e65c2b5c300ab92320c4dbf702b0b8a
Thoma411 <NADTM_Thomax11@163.com>
2023-05-24 16:13:32 +0800

    添加文本(明文密文显示)

--- kerb_main/C_ui.py
+++ kerb_main/C_ui.py
@@ -1,13 +1,12 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 import tkinter.font as tkFont
 # from MsgFieldDef import *
 import C_Tclient as cc
 from tkinter import *
+import os
+import time
 
-# K_CV = cc.DKEY_C
-# Vsock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-# #Vsock.connect((V_IP, V_PORT))
 k_cv = None
 C_PKEY_V = None
 Vsock = None
@@ -646,3 +645,58 @@ class AdminManage:
                 ))  # 修改对于行信息
             else:
                 messagebox.showinfo('警告！', '不能修改学生学号！')
+
+
+class TextFileReader(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title('实时文本阅读器')
+
+        self.text1 = tk.Text(self, wrap=tk.WORD)
+        self.text1.pack(expand=True, fill=tk.BOTH)
+
+        self.text2 = tk.Text(self, wrap=tk.WORD)
+        self.text2.pack(expand=True, fill=tk.BOTH)
+
+        button = tk.Button(self, text='打开文件1', command=self.open_file_1)
+        button.pack(fill=tk.X)
+
+        button2 = tk.Button(self, text='打开文件2', command=self.open_file_2)
+        button2.pack(fill=tk.X)
+
+        self.file_path_1 = None
+        self.file_path_2 = None
+
+        self.text_last_update_time_1 = None
+        self.text_last_update_time_2 = None
+
+        self.poll_file_changes()
+
+    def load_text(self, file_path, text_widget):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        text_widget.delete(1.0, tk.END)
+        text_widget.insert(tk.END, content)
+
+    def open_file_1(self):
+        self.file_path_1 = filedialog.askopenfilename()
+        self.text_last_update_time_1 = time.time()
+        self.load_text(self.file_path_1, self.text1)
+
+    def open_file_2(self):
+        self.file_path_2 = filedialog.askopenfilename()
+        self.text_last_update_time_2 = time.time()
+        self.load_text(self.file_path_2, self.text2)
+
+    def poll_file_changes(self):
+        if self.file_path_1:
+            if os.path.getmtime(self.file_path_1) >= self.text_last_update_time_1:
+                self.load_text(self.file_path_1, self.text1)
+
+        if self.file_path_2:
+            if os.path.getmtime(self.file_path_2) >= self.text_last_update_time_2:
+                self.load_text(self.file_path_2, self.text2)
+
+        self.after(2000, self.poll_file_changes)
